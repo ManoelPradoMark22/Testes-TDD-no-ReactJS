104c22a33b6d9640571a60df106988463a2a09c1
Manoel de Oliveira Prado Neto <manoelprado.aecjr@gmail.com>
2019-12-11 14:59:51 -0300

    testing form

--- src/__tests__/components/TechList.test.js
+++ src/__tests__/components/TechList.test.js
@@ -7,24 +7,26 @@ import Techlist from '~/components/TechList';
 
 describe('Techlist component', () => {
   it('should be able to add new tech', () => {
-    const { getByText, getByTestId, debug } = render(<Techlist />);
+    const { getByText, getByTestId, getByLabelText } = render(<Techlist />);
+    /*getByLabelText para pegar o input. ele procura pelo id do input relacionado
+    com o htmlFor da label em questao e pega este input! */
+    /*demonstrando: 
+    <label htmlFor="tech">tech</label>
+    <input id="tech" onChange={e => e.target.value} />*/
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
 
-    debug();
+    /*esse submit, ao contario do change, nao precisa de nenhum parâmetro  */
+    fireEvent.submit(getByTestId('tech-form'));
 
-    /*getByText vai no componente com o texto em questão! */
-    fireEvent.click(getByText('Adicionar'));
-
-    debug();
-
-    /*assim não fica tao bom, pq vai procurar pór qualquer texto do tipo,
-    mesmo se nao for um item da nossa lista! 
-    expect(getByText('Node.js')).toBeTruthy(); 
-    
-    entao por isso vamos usar tambem o getByTestId para pegarmos os elementos
+    /*vamos usar tambem o getByTestId para pegarmos os elementos
     que tenham o id data-testid="tech-list", assim teremos certeza que estamos verificando apenas
     os elementos da nossa lista!*/
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
     /*tradução: eu espero, que a minha lista 'tech-list' tenha um elemento que
     esteja escrito 'Node.js' */
+
+    expect(getByLabelText('Tech')).toHaveValue('');
+    /*esperamos que o nosso input de tecnologia esteja vazio após adicionarmos 
+    uma tech */
   });
 });
